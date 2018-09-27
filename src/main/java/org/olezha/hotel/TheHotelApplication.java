1034d8c31f626a58d3599c9ca5e7db25da38808b
Oleh Shklyar <oleh.shklyar@gmail.com>
2018-09-27 22:35:41 +0300

    start spring

---
+++ src/main/java/org/olezha/hotel/TheHotelApplication.java
@@ -0,0 +1,12 @@
+package org.olezha.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheHotelApplication.class, args);
+    }
+}
