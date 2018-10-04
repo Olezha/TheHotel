43eb1262d163a0755b443d3c56a4ca01e740c024
Oleh <pio_ua@ukr.net>
2018-10-04 18:40:26 +0300

    js&css

--- src/main/java/ua/olezha/hotel/TheHotelApplication.java
+++ src/main/java/ua/olezha/hotel/TheHotelApplication.java
@@ -3,9 +3,14 @@ package ua.olezha.hotel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class TheHotelApplication {
@@ -23,7 +28,7 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/management/**").hasRole("HOTELIER")
+                .antMatchers("/management/**", "/h2-console/**").hasRole("HOTELIER")
                 .anyRequest().permitAll()
 
                 .and()
@@ -37,5 +42,16 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .headers()
                 .frameOptions()
                 .disable();
+    }
+}
+
+@Configuration
+class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/ext/**")
+                .addResourceLocations("/webjars/")
+                .setCacheControl(CacheControl.maxAge(30L, TimeUnit.DAYS).cachePublic());
     }
 }
